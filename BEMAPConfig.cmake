d14c2ca6c1f7d61e163f668229db26b25d7ec626
Yuri Ardila <ardiyu07@gmail.com>
2015-10-19 06:01:22 +0900

    Removed arch_10 from cmakelist

--- BEMAPConfig.cmake
+++ BEMAPConfig.cmake
@@ -17,7 +17,7 @@ IF(BUILD_CUDA)
     )
   
   # Set NVCC Flags
-  SET(CUDA_NVCC_FLAGS -O3 -gencode arch=compute_10,code=sm_10 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -lpthread) 
+  SET(CUDA_NVCC_FLAGS -O3 -gencode arch=compute_20,code=sm_20 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -lpthread) 
 ENDIF()
 
 # Set flag
